ad69250ac4894a5b316529b20cda041fbced0276
Diego Silva <diego23montes@outlook.com>
2025-07-13 22:34:12 -0600

    cambios en users.py: cambiando nombre de rutas para la conservacion de convencion REST

--- src/APIRouter_Proyecto/Routers/users.py
+++ src/APIRouter_Proyecto/Routers/users.py
@@ -23,7 +23,7 @@ class DeleteResponse(BaseModel):
     mensaje : str
     usuario : User
 
-@router.post("/crear_usuario", summary="Crear un usuario", response_model=User, 
+@router.post("/", summary="Crear un usuario", response_model=User, 
             response_description="Modelo User",
             status_code=(status.HTTP_201_CREATED))
 async def crear_usuario(user: User):
@@ -35,12 +35,12 @@ async def crear_usuario(user: User):
     return user_dict
 
 
-@router.get("/lista", summary="ver lista de usuario", response_model=list[dict])
+@router.get("/", summary="ver lista de usuario", response_model=list[User])
 async def ver_usuario():
     return user_db
 
 
-@router.get("/lista/{user_id}", summary="ver usuario por id", response_model=User,
+@router.get("/{user_id}", summary="ver usuario por id", response_model=User,
             response_description="modelo usuario")
 async def usuario_por_id(user_id:int):
     for user_item in user_db:
@@ -48,7 +48,7 @@ async def usuario_por_id(user_id:int):
             return user_item
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
 
-@router.put("/actualizar_usuario/{user_id}", summary="Actualizar usuario", response_model=User)
+@router.put("/{user_id}", summary="Actualizar usuario", response_model=User)
 async def actualizar_usuario(user_id:int ,updated_user:User):
     for user_item in user_db:
         if user_item["id"] == user_id:
@@ -56,9 +56,9 @@ async def actualizar_usuario(user_id:int ,updated_user:User):
             user_item["email"] = updated_user.email
             user_item["age"] = updated_user.age
             return user_item
-    raise HTTPException(status_code=404, detail="Tarea no encontrada")
+    raise HTTPException(status_code=404, detail="Usuario no encontrada")
 
-@router.delete("/borrar_usuario/{user_id}", summary="Borrar usuario", response_model=DeleteResponse)
+@router.delete("/{user_id}", summary="Borrar usuario", response_model=DeleteResponse)
 async def borrar_usuario(user_id:int):
     for idx, user_item in enumerate(user_db):
         if user_item["id"] == user_id:
