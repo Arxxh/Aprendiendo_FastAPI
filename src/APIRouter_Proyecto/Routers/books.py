fdf3480e82cee39396a7cc945bf9de6726f96a35
Diego Silva <diego23montes@outlook.com>
2025-07-14 20:22:42 -0600

    add books.py CRUD Create books read books (mostrar lista y mostrar lista por id)

--- src/APIRouter_Proyecto/Routers/books.py
+++ src/APIRouter_Proyecto/Routers/books.py
@@ -23,6 +23,25 @@ class DeleteResponse(BaseModel):
     book: Book
 
 # crear un libro
-@router.post("/", summary="crear un libro", response_model=Book, status_code=status.HTTP_201_CREATED)
-async def crear_libro():
-    return
+@router.post("/", summary="crear un libro",
+              response_model=Book, status_code=status.HTTP_201_CREATED)
+async def crear_libro(book:Book):
+    global next_id_books
+    book_dict = book.model_dump()
+    book_dict["id"] = next_id_books
+    books_db.append(book_dict)
+    next_id_books += 1
+    return book_dict
+
+@router.get("/", summary="mostrar libros")
+async def ver_libros():
+    return books_db
+
+@router.get("/{libro_id}", summary="ver libro por id", response_model=Book)
+async def libro_por_id(libro_id:int):
+    for libro_item in books_db:
+        if libro_item["id"] == libro_id:
+            return libro_item
+    raise HTTPException(status_code=404, detail="Libro no encontrado")
+
+
