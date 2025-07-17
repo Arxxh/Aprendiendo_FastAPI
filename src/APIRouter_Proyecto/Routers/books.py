d92786e9a86f5bac8c76d9bdf4278e583d730a14
Diego Silva <diego23montes@outlook.com>
2025-07-16 18:25:11 -0600

    add update and deleted routes for book api

--- src/APIRouter_Proyecto/Routers/books.py
+++ src/APIRouter_Proyecto/Routers/books.py
@@ -45,3 +45,23 @@ async def libro_por_id(libro_id:int):
     raise HTTPException(status_code=404, detail="Libro no encontrado")
 
 
+@router.put("/{libro_id}", summary="actualizar libro", response_model=Book)
+async def update_book(libro_id:int, updated_book: Book):
+    for book_item in books_db:
+        if book_item["id"] == libro_id:
+            book_item["title"] = updated_book.title
+            book_item["author"] = updated_book.author
+            book_item["year"] = updated_book.year
+            book_item["available"] = updated_book.available
+            return book_item
+    raise HTTPException(status_code=404, detail="libro no encontrado")
+
+@router.delete("/{libro_id}", summary="borrar libro", response_model=DeleteResponse)
+async def delete_book(libro_id:int):
+    for idx, book_item in enumerate(books_db):
+        if book_item["id"] == libro_id:
+            deleted = books_db.pop(idx)
+            return {"mensaje": "Libro eliminado", "book": Book(**deleted)}
+    raise HTTPException(status_code=404, detail="libro no encontrado")
+
+
